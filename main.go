158f5c0ee2fe3f591bb38742c41f25e8a1d23d4d
rizalherlangga <rizsut03@example.com>
2023-07-22 19:03:05 +0700

    task 12

--- main.go
+++ main.go
@@ -11,17 +11,58 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type project struct {
+	Title       string
+	Start       string
+	End         string
+	Duration    string
+	Description string
+	Tech        []string
+}
+
+var dataProject = []project{
+	{
+		Title:       "FAKTA BANGET NICH !",
+		Start:       "24/06/2023",
+		End:         "27/06/2023",
+		Duration:    "3 hari",
+		Description: "MAU TAU FAKTA GA BRO ? RAHASIA DAPUR NIH !",
+		Tech:        []string{"js", "bootstrap", "golang", "react"},
+	},
+	{
+		Title:       "GOKIL SIH INI CUY !",
+		Start:       "24/06/2023",
+		End:         "27/06/2023",
+		Duration:    "3 hari",
+		Description: "LU KEREN GUA KEREN, KITA BERDUA ? KEREN !",
+		Tech:        []string{"js", "bootstrap", "golang", "react"},
+	},
+	{
+		Title:       "AZEEEEKKKK !",
+		Start:       "24/06/2023",
+		End:         "27/06/2023",
+		Duration:    "3 hari",
+		Description: "WAHHH KEREN BANGET LU BRAYY, RESCPECT !",
+		Tech:        []string{"js", "bootstrap", "golang", "react"},
+	},
+}
+
 func main() {
 	e := echo.New()
 
 	e.Static("/public", "public")
 
 	e.GET("/", home)
+	e.GET("/myProject", myProject)
 	e.GET("/contact", contact)
-	e.GET("/project", project)
+	e.GET("/project-detail/:id", projectDetail)
 	e.GET("/testimonial", testimonial)
-	e.GET("/projectdetail:id", projectDetail)
-	e.POST("addProject", addProject)
+	e.GET("/project", addProject)
+
+	e.POST("/edit-project/:id", editProject)
+	e.POST("/delete-project/:id", deleteProject)
+	e.POST("/addedProject", addedProject)
+	e.POST("/project", addProject)
 
 	e.Logger.Fatal(e.Start("localhost:5000"))
 }
@@ -35,27 +76,21 @@ func home(c echo.Context) error {
 
 	return tmpl.Execute(c.Response(), nil)
 }
+func myProject(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/my-project.html")
 
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	projects := map[string]interface{}{
+		"projects": dataProject,
+	}
+
+	return tmpl.Execute(c.Response(), projects)
+}
 func contact(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/contact.html")
-
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
-	}
-
-	return tmpl.Execute(c.Response(), nil)
-}
-func project(c echo.Context) error {
-	var tmpl, err = template.ParseFiles("views/project.html")
-
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
-	}
-
-	return tmpl.Execute(c.Response(), nil)
-}
-func testimonial(c echo.Context) error {
-	var tmpl, err = template.ParseFiles("views/testimonial.html")
 
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
@@ -66,10 +101,23 @@ func testimonial(c echo.Context) error {
 func projectDetail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	data := map[string]interface{}{
-		"Id":      id,
-		"Title":   "Dumbways Web App",
-		"Content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,placeat recusandae, doloremque vel et in impedit omnis velit idporro animi, optio alias dolorem. Beatae a facilis earum!Praesentium at voluptates amet assumenda soluta earum eos nesciunt		eaque. Dolorum recusandae quo et delectus eius optio temporibusdebitis voluptates sed nam?",
+	var ProjectDetail = project{}
+
+	for index, item := range dataProject {
+		if id == index {
+			ProjectDetail = project{
+				Title:       item.Title,
+				Start:       item.Start,
+				End:         item.End,
+				Duration:    item.Duration,
+				Description: item.Description,
+				Tech:        item.Tech,
+			}
+		}
+	}
+
+	item := map[string]interface{}{
+		"Project": ProjectDetail,
 	}
 
 	var tmpl, err = template.ParseFiles("views/project-detail.html")
@@ -78,20 +126,38 @@ func projectDetail(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
 
-	return tmpl.Execute(c.Response(), data)
+	return tmpl.Execute(c.Response(), item)
 }
+func testimonial(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/testimonial.html")
 
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
 func addProject(c echo.Context) error {
-	nama := c.FormValue("input-nama")
+	var tmpl, err = template.ParseFiles("views/project.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
+}
+func addedProject(c echo.Context) error {
+	name := c.FormValue("input-nama")
 	start := c.FormValue("input-start")
 	end := c.FormValue("input-end")
-	description := c.FormValue("input-deskripsi")
-	checkbox := c.Request().Form["tech"]
+	desc := c.FormValue("input-deskripsi")
+	Tech := c.Request().Form["Tech"]
 
 	startDate, err := time.Parse("2006-01-02", start)
 	if err != nil {
 		return err
 	}
+
 	endDate, err := time.Parse("2006-01-02", end)
 	if err != nil {
 		return err
@@ -99,6 +165,12 @@ func addProject(c echo.Context) error {
 
 	duration := int(endDate.Sub(startDate).Hours() / 24)
 	var durationOutput string
+
+	fmt.Println("name : ", name)
+	fmt.Println("start : ", start)
+	fmt.Println("end : ", end)
+	fmt.Println("desc : ", desc)
+	fmt.Println("Technologies : ", strings.Join(Tech, ", "))
 
 	if duration <= 7 {
 		durationOutput = fmt.Sprintf("%d day(s)", duration)
@@ -114,12 +186,38 @@ func addProject(c echo.Context) error {
 		durationOutput = fmt.Sprintf("%d year(s)", years)
 	}
 
-	fmt.Println("name : " + nama)
-	fmt.Println("start : " + start)
-	fmt.Println("end : " + end)
 	fmt.Println("Duration: ", durationOutput)
-	fmt.Println("description : " + description)
-	fmt.Println("Technologies : ", strings.Join(checkbox, ", "))
 
-	return c.Redirect(http.StatusMovedPermanently, "/project")
+	var newProject = project{
+		Title:       name,
+		Start:       start,
+		End:         end,
+		Duration:    durationOutput,
+		Description: desc,
+		Tech:        Tech,
+	}
+
+	dataProject = append(dataProject, newProject)
+
+	fmt.Println(dataProject)
+
+	return c.Redirect(http.StatusMovedPermanently, "/myProject")
+}
+func deleteProject(delete echo.Context) error {
+	i, _ := strconv.Atoi(delete.Param("id"))
+
+	fmt.Println("index : ", i)
+
+	dataProject = append(dataProject[:i], dataProject[i+1:]...)
+
+	return delete.Redirect(http.StatusMovedPermanently, "/myProject")
+}
+
+func editProject(edit echo.Context) error {
+	i, _ := strconv.Atoi(edit.Param("id"))
+
+	fmt.Println("index : ", i)
+
+	dataProject = append(dataProject[:i], dataProject[i+1:]...)
+	return edit.Redirect(http.StatusMovedPermanently, "/project")
 }
